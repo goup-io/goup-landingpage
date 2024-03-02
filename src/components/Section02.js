751f325947825809d6977e407ef520cf33ea0e93
LccsBueno <luccas.lima@sptech.school>
2024-03-02 16:14:19 -0300

    Adicionando a animaçao do foguete

--- src/components/Section02.js
+++ src/components/Section02.js
@@ -1,10 +1,14 @@
 import Foguete from '../assets/foguete.png'
+import Fogo from '../assets/fogo.png'
 import '../index.css';
 
 function Section02(){
     return (
         <section id="sobre" class="flex flex-row justify-center gap-20">
-            <img src={`${Foguete}`}></img>
+            <div class="">
+                <img src={`${Foguete}`} class="animate-foguete"></img>
+                <img src={`${Fogo}`} class="mx-20 -mt-14 animate-fogo"></img>
+            </div>
             <div class="flex flex-col justify-center items-center w-2/6">
                 <p class="text-7xl">Com a gente seu negócio <span className="letraDestaqueVerde">decola</span></p>
                 <p class="font-extralight mt-10">Oferecemos soluções personalizadas para seus problemas</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,29 @@ module.exports = {
       "Poppins" : ["Poppins", "sans-serif"]
     },
     extend: {
+      animation: {
+        'foguete': 'tremer 0.6s linear infinite',
+        'fogo': 'tremer 0.3s linear infinite',
+      },
+      keyframes : {
+        tremer : {
+          '0%' : { transform: 'translate(1px, 1px) rotate(0deg)'},
+          '10%' : { transform: 'translate(-1px, -2px) rotate(1deg)'},
+          '20%' : { transform: 'translate(-1px, 0px) rotate(1deg)'},
+          '30%' : { transform: 'translate(1px, 2px) rotate(0deg)'},
+          '40%' : { transform: 'translate(1px, -1px) rotate(1deg)'},
+          '50%' : { transform: 'translate(-1px, 2px) rotate(1deg)'},
+          '60%' : { transform: 'translate(-1px, 1px) rotate(0deg)'},
+          '70%' : { transform: 'translate(1px, 1px) rotate(1deg)'},
+          '80%' : { transform: 'translate(-1px, -1px) rotate(0deg)'},
+          '90%' : { transform: 'translate(1px, 1px) rotate(1deg)'},
+          '100%' : { transform: 'translate(1px, -1px) rotate(0deg)'},
+        },
+        pulsar : {
+          '0%': { transform : ''},
+          '50%': { transform : 'scale(1.1)'},
+        }
+      },
       colors: {
         'verdeGoUp' : '#48CFAE',
         'brancoGoUp' : '#F1F1F1',
