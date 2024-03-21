d6cc8e6161166e2663d6125ddeaaa1f4bb25dbca
InacioFigueiredo13 <inaciofigueiredo13@gmail.com>
2024-03-21 16:17:29 -0300

    Adicionando o build

--- src/components/Sections/Section01.js
+++ src/components/Sections/Section01.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import MediaQuery from 'react-responsive';
 import "../functions/TextAnimation.css"
 
