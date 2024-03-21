81a9d9192281140ae521dd35b524fcbf81ea2fdb
LccsBueno <luccas.lima@sptech.school>
2024-03-21 12:19:51 -0300

    Subindo v3 da animacao da section01

--- src/components/Sections/Section01.js
+++ src/components/Sections/Section01.js
@@ -6,12 +6,14 @@ const Section01 = () => {
   return (
     <>
       <MediaQuery minWidth={1025}>
+
+        <MediaQuery maxWidth={1554}>
         <section id="sobre" className="flex flex-col font-Poppins text-center">
           <p className="titulo">
             Em direção ao <span className="letraDestaqueVerde">Futuro</span>
           </p>
 
-        <div className='containerTextoAnimacao'>
+        <div className='containerTextoAnimacaoMax1554'>
             <div>
                 <p class="w-7/12 text-4xl mt-20">Construimos aplicações que mudam a sociedade</p>
             </div>
@@ -30,6 +32,34 @@ const Section01 = () => {
 
         </div>
         </section>
+        </MediaQuery>
+
+        <MediaQuery minWidth={1555}>
+        <section id="sobre" className="flex flex-col font-Poppins text-center">
+          <p className="titulo">
+            Em direção ao <span className="letraDestaqueVerde">Futuro</span>
+          </p>
+
+        <div className='containerTextoAnimacaoMin1555'>
+            <div>
+                <p class="w-7/12 text-4xl mt-20">Construimos aplicações que mudam a sociedade</p>
+            </div>
+            <div>
+                <p class="w-7/12 text-4xl mt-20">Valorizamos relacionamentos baseados em confiança, respeito e transparência.</p>
+            </div>
+            <div>
+                <p class="w-7/12 text-4xl mt-20">Agimos com integridade e ética, assumindo a responsabilidade pelo impacto de nossas ações.</p>
+            </div>
+            <div>
+                <p class="w-7/12 text-4xl mt-20">Buscamos constantemente novas maneiras de impulsionar o progresso tecnológico.</p>
+            </div>
+            <div>
+                <p class="w-7/12 text-4xl mt-20">Construimos aplicações que mudam a sociedade</p>
+            </div>
+
+        </div>
+        </section>
+        </MediaQuery>
       </MediaQuery>
     </>
   );
